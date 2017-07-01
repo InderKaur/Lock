4ac91f35e50a72878ecbece39204871479a6eac5
InderKaur <kaurneetu0001@gmail.com>
2017-07-01 14:00:02 +0530

    Locking Screen

---
+++ app/src/main/java/com/calculator/MyAdminReceiver.java
@@ -0,0 +1,10 @@
+package com.calculator;
+
+import android.app.admin.DeviceAdminReceiver;
+
+/**
+ * Created by HP on 29-06-2017.
+ */
+
+public class MyAdminReceiver extends DeviceAdminReceiver {
+}
